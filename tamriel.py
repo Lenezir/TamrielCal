d4b9dd65e3ca9fcc93516d3c4e06c1439a8178a8
Lenezir <github@lenezir.fr>
2017-11-29 18:22:48 +0100

    Ajout des saisons

--- tamriel.py
+++ tamriel.py
@@ -11,13 +11,14 @@ import calendar
 moistam = ("Primétoile", "Clairciel", "Semailles", "Ondepluie", "Plantaisons", "Mi-l'An", "Hautzénith", "Vifazur", "Âtrefeu", "Soufflegivre", "Sombreciel", "Soirétoile")
 jourtam = ("Morndas", "Tirdas", "Middas", "Turdas", "Fredas", "Loredas", "Sundas")
 astrotam = ("le Rituel", "l'Amant", "le Seigneur", "le Mage", "l'Ombre", "le Destrier", "l'Apprenti", "le Guerrier", "la Dame", "la Tour", "l'Astronach", "le Voleur")
+saisontam = ("hiver","printemps","printemps","printemps","été","été","été","automne","automne","automne","hiver","hiver")
 
 # Récupération de la date du moment
 date = datetime.datetime.now()
 now = datetime.datetime.today()
 
 # Affichage de la date du jour
-print("Nous sommes le", jourtam[now.weekday()], date.day, moistam[date.month-1], date.year)
+print("Nous sommes le", jourtam[now.weekday()], date.day, moistam[date.month-1], date.year, "et nous somme en plein", saisontam[date.month-1], ".")
 
 ########################################################################################################################
 
@@ -33,9 +34,9 @@ jour = int(dateob[0])
 # Définition du jour de semaine d'une date donnée
 c = calendar.Calendar()
 
+# Boucle permettant de trouver le jour de la semaine 
 for i in c.itermonthdays2(annee,mois+1):
 	if(i[0] == jour):
 		res = i[1]
-		print(res)
 
-print("Vous êtes né le", jourtam[res], jour, moistam[mois], annee, ". Votre signe est", astrotam[mois] +".")
+print("Vous êtes né le", jourtam[res], jour, moistam[mois], annee, "en plein",saisontam[mois-1],". Votre signe est", astrotam[mois] +".")
