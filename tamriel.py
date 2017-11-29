9736a8c6a63dda50d1e86ddb5c78a67bdd3a51fc
Lenezir <github@lenezir.fr>
2017-11-29 17:41:40 +0100

    Ajout de fonctions

--- tamriel.py
+++ tamriel.py
@@ -1,21 +1,41 @@
 """
 TamrielCal v0.1
-Calendrier de correspondance entre le calendrier grégorien et le calendrier de Tamriel.
+Calendrier de correspondance entre le calendrier grégorien et le celui de Tamriel.
 """
 
+# Import de bibliothèques
 import datetime
+import calendar
 
-mois = ("Primétoile", "Clairciel", "Semailles", "Ondepluie", "Plantaisons", "Mi-l'An", "Hautzénith", "Vifazur", "Âtrefeu", "Soufflegivre", "Sombreciel", "Soirétoile")
-jours = ("Morndas", "Tirdas", "Middas", "Turdas", "Fredas", "Loredas", "Sundas")
-astro = ("le Rituel", "l'Amant", "le Seigneur", "le Mage", "l'Ombre", "le Destrier", "l'Apprenti", "le Guerrier", "la Dame", "la Tour", "l'Astronach", "le Voleur")
+# Jours, mois et signes astrologiques de Tamriel
+moistam = ("Primétoile", "Clairciel", "Semailles", "Ondepluie", "Plantaisons", "Mi-l'An", "Hautzénith", "Vifazur", "Âtrefeu", "Soufflegivre", "Sombreciel", "Soirétoile")
+jourtam = ("Morndas", "Tirdas", "Middas", "Turdas", "Fredas", "Loredas", "Sundas")
+astrotam = ("le Rituel", "l'Amant", "le Seigneur", "le Mage", "l'Ombre", "le Destrier", "l'Apprenti", "le Guerrier", "la Dame", "la Tour", "l'Astronach", "le Voleur")
 
+# Récupération de la date du moment
 date = datetime.datetime.now()
 now = datetime.datetime.today()
 
-print("Nous sommes le",jours[now.weekday()], date.day, mois[date.month-1], date.year)
+# Affichage de la date du jour
+print("Nous sommes le", jourtam[now.weekday()], date.day, moistam[date.month-1], date.year)
 
-dob = int(input("Entrez votre jour de naissance : "))
-mob = int(input("Entrez le chiffre correspondant au mois de naissance (ex. 6 pour juin) : "))
-yob = int(input("Entrez votre année de naissance : "))
+########################################################################################################################
 
-print("Vous êtes né le", dob, mois[mob-1], yob,". Votre signe est", astro[mob-1],".")
+# L'utilisateur entre sa date de naissance
+entree = input("Entrez votre date de naissance (au format jj/mm/aaaa) : ")
+
+# Découpage de la date entrée en jour/mois/année
+dateob = entree.split('/')
+annee = int(dateob[2])
+mois = int(dateob[1])-1
+jour = int(dateob[0])
+
+# Définition du jour de semaine d'une date donnée
+c = calendar.Calendar()
+for i in c.itermonthdays2(annee,mois+1):
+    print(i)
+    res = i in i[0] == jour
+
+
+
+print("Vous êtes né le", jour, moistam[mois], annee, ". Votre signe est", astrotam[mois] +".")
