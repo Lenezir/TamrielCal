383f74bd7a629cc4e53ec528090e4d139818f9f8
Lenezir <github@lenezir.fr>
2017-12-03 16:19:01 +0100

    Suppression de certaines variables inutiles, afinement du code.

--- tamriel.py
+++ tamriel.py
@@ -16,10 +16,21 @@ saisontam = ("printemps", "été", "automne", "hiver")
 # Récupération de la date du moment
 date = datetime.datetime.now()
 now = datetime.datetime.today()
-moisactuel = moistam[date.month-1]
-saison = moisactuel[1]
+
+# Boucle pour ajouter 'er' au premier jour du mois
+if(date.day == 1):
+	auj = "1er"
+else:
+	auj = date.day
+
+# Boucle pour le choix de l'article
+if(saisontam[moistam[date.month-1][1]] == "printemps"):
+	article = "au"
+else:
+	article = "en"
+
 # Affichage de la date du jour
-print("Nous sommes le", jourtam[now.weekday()], date.day, moisactuel[0], date.year, "et nous sommes en plein", saisontam[saison], ".")
+print("Nous sommes le ", jourtam[now.weekday()]," ", auj," ", moistam[date.month-1][0]," ", date.year, ", nous sommes ", article," ", saisontam[moistam[date.month-1][1]], " et le signe astrologique du mois est ", astrotam[date.month-1], ".", sep='')
 
 ########################################################################################################################
 
@@ -32,7 +43,11 @@ annee = int(dateob[2])
 mois = int(dateob[1])-1
 jour = int(dateob[0])
 
-moistamactuel = moistam[mois]
+# Boucle pour ajouter 'er' au premier jour du mois
+if(jour == 1):
+	jour_nd = "1er"
+else:
+	jour_nd = jour
 
 # Définition du jour de semaine d'une date donnée
 c = calendar.Calendar()
@@ -42,4 +57,10 @@ for i in c.itermonthdays2(annee,mois+1):
 	if(i[0] == jour):
 		res = i[1]
 
-print("Vous êtes né le", jourtam[res], jour, moistamactuel[0], annee, "en plein",saisontam[mois-1],". Votre signe est", astrotam[mois] +".")
+# Article à mettre devant la saison (au à la place de en pour le printemps)
+if(saisontam[moistam[mois][1]] == "printemps"):
+	article = "au"
+else:
+	article = "en"
+
+print("Vous êtes né le ", jourtam[res]," ", jour_nd," ", moistam[mois][0]," ", annee," ", article," ", saisontam[moistam[mois][1]], ". Votre signe est ", astrotam[mois], ".", sep='')
